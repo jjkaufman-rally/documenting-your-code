175f77d881d6e05b48bf97e763ee6ded75a6c524
JJ Kaufman <jj.kaufman@rallyhealth.com>
2022-04-28 14:40:58 -0600

    comments added

--- randomly/password.py
+++ randomly/password.py
@@ -1,16 +1,33 @@
 import random
 import string
+from typing import Optional
 
 
-def generate_password(chars, punctuation, invalid_chars=None):
+def generate_password(
+    chars: int,
+    punctuation: bool,
+    invalid_chars: Optional[list] = None
+) -> string:
+    """
+    Create a string of random characters
+    Args:
+        chars: number of characters in return string
+        punctuation: whether to use punctuation characters
+        invalid_chars: list of characters not to use
+
+    Returns:
+        password: string of random characters
+    """
     valid_chars = string.ascii_letters + string.digits
 
+    # Only include punctuation characters if specified.
     if punctuation:
         valid_chars += string.punctuation
 
     for invalid_char in invalid_chars:
         valid_chars = valid_chars.replace(invalid_char, "")
 
+    # creates a list of characters randomly chosen and then turns them into a string
     password_chars = random.choices(valid_chars, k=chars)
     password = "".join(char for char in password_chars)
 
